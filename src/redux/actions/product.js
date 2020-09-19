fb371168c73f030ec53f45acd23f96ccdcf677ed
olegshevt <olegshevt@gmail.com>
2020-09-19 18:15:54 +0300

    Turn off server 3

--- src/redux/actions/product.js
+++ src/redux/actions/product.js
@@ -7,7 +7,7 @@ export const setLoaded = (payload) => ({
 
 export const fetchProduct = (sortBy, category) => (dispatch) => {
     dispatch(setLoaded(false));
-    axios.get(`/db.json/pizzas?${
+    axios.get(`https://react-shopping-cart-nine-weld.vercel.app/db.json/pizzas?${
         category !== null ? `category=${category}` : ''
         }&_sort=${sortBy.type}&_order=${sortBy.order}`,
     )
