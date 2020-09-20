86c566af98e45a6ec6b1caaad54d7fdcf6b9387f
olegshevt <olegshevt@gmail.com>
2020-09-20 06:46:53 +0300

    Created build

---
+++ build/precache-manifest.00b5dc106f3ff6587e4a6faaf66f9ecb.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839971af3ead4b7e6ffc91c13d4887a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e4ef344bbd34383a8d8",
+    "url": "/static/css/main.f0a0bb61.chunk.css"
+  },
+  {
+    "revision": "748c2307df3c4700cc90",
+    "url": "/static/js/2.7f700e8c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7f700e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4ef344bbd34383a8d8",
+    "url": "/static/js/main.b9df834c.chunk.js"
+  },
+  {
+    "revision": "9aa64a36a844813225ab",
+    "url": "/static/js/runtime-main.fdfc6804.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "ceb2faf089a96bede51ccde3693d012b",
+    "url": "/static/media/empty-cart.ceb2faf0.png"
+  },
+  {
+    "revision": "bebbda5c11affe0c0b315f811f252581",
+    "url": "/static/media/logo_coffee.bebbda5c.png"
+  }
+]);
