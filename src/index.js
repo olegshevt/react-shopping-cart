68cc9311f9c32d2c1b57af4d5eadbea66f065bb2
olegshevt <olegshevt@gmail.com>
2020-09-21 15:10:17 +0300

    up1

--- src/index.js
+++ src/index.js
@@ -6,13 +6,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
-}
-
 ReactDOM.render(
   <Router>
     <Provider store={store}>
