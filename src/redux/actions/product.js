015a375eb3323573a6257b6003514f1c934c31dd
olegshevt <olegshevt@gmail.com>
2020-09-21 14:50:56 +0300

    update11

--- src/redux/actions/product.js
+++ src/redux/actions/product.js
@@ -9,9 +9,9 @@ export const fetchProduct = (sortBy, category) => (dispatch) => {
   dispatch(setLoaded(false));
   axios
     .get(
-      `https://coffee-shopping-cart.herokuapp.com/db.json/pizzas?${
-        category !== null ? `category=${category}` : ''
-      }&_sort=${sortBy.type}&_order=${sortBy.order}`,
+      `/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${
+        sortBy.order
+      }`,
     )
     .then(({ data }) => {
       dispatch(setProduct(data));
